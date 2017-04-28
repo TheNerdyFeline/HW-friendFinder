fba17596857b2d19b35c1f1d60996b5ee1be6170
Jackie Wellons <jackie@localhost.localdomain>
2017-04-28 16:13:20 -0700

    not pushing newFriend to friends.js

--- data/friends.js
+++ data/friends.js
@@ -1,4 +1,11 @@
-var friendsArr = [];
+var friendsArr = [
+    {
+	name: "Kenzi",
+	Photo: "https://pbs.twimg.com/profile_images/757615677393940480/xmmbRon8.jpg",
+	Scores: [4, 3, 3, 4, 2, 5, 3, 4, 5, 2]
+    }
+];
+console.log(friendsArr);
 module.exports = friendsArr;
 
 
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,11 @@
-var friends = require("../data/friends.js");
+var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-	res.json(friends);
+	res.json(friendsData);
     });
 
     app.post("api/friends", function(req, res) {
-	friends.push(req.body);
+	friendsData.push(req.body);
     });
 };
